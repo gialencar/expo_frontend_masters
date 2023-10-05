af5e560022dfe8080497625ba07a71665afd1390
Gilson Alencar <gilson.alencar94@gmail.com>
2023-10-04 23:50:11 -0300

    lists exercise

--- App.js
+++ App.js
@@ -1,27 +1,44 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import ColorBox from './components/ColorBox';
+
+const COLORS = [
+  { colorName: 'Base03', hexCode: '#002b36' },
+  { colorName: 'Base02', hexCode: '#073642' },
+  { colorName: 'Base01', hexCode: '#586e75' },
+  { colorName: 'Base00', hexCode: '#657b83' },
+  { colorName: 'Base0', hexCode: '#839496' },
+  { colorName: 'Base1', hexCode: '#93a1a1' },
+  { colorName: 'Base2', hexCode: '#eee8d5' },
+  { colorName: 'Base3', hexCode: '#fdf6e3' },
+  { colorName: 'Yellow', hexCode: '#b58900' },
+  { colorName: 'Orange', hexCode: '#cb4b16' },
+  { colorName: 'Red', hexCode: '#dc322f' },
+  { colorName: 'Magenta', hexCode: '#d33682' },
+  { colorName: 'Violet', hexCode: '#6c71c4' },
+  { colorName: 'Blue', hexCode: '#268bd2' },
+  { colorName: 'Cyan', hexCode: '#2aa198' },
+  { colorName: 'Green', hexCode: '#859900' },
+];
 
 const App = () => {
   return (
     <SafeAreaView style={styles.safeArea}>
-      <View style={[styles.container]}>
-        <Text style={styles.heading}>
-          Here are some boxes of different colors
-        </Text>
-        <ColorBox colorName={'Cyan'} ColorHex={'#2aa198'} />
-        <ColorBox colorName={'Blue'} ColorHex={'#268bd2'} />
-        <ColorBox colorName={'Magenta'} ColorHex={'#d33682'} />
-        <ColorBox colorName={'Orange'} ColorHex={'#cb4b16'} />
-      </View>
+      <FlatList
+        data={COLORS}
+        renderItem={({ item }) => (
+          <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
+        )}
+        keyExtractor={(item) => item.hexCode}
+        ListHeaderComponent={<Text style={styles.heading}>Solarized</Text>}
+      />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
     justifyContent: 'center',
     paddingHorizontal: 20,
     gap: 10,
@@ -33,6 +50,9 @@ const styles = StyleSheet.create({
   },
   safeArea: {
     flex: 1,
+  },
+  listContainer: {
+    width: '100%',
   },
 });
 
--- components/ColorBox.js
+++ components/ColorBox.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const ColorBox = ({ colorName, ColorHex }) => {
+const ColorBox = ({ colorName, hexCode }) => {
   const boxColor = {
-    backgroundColor: ColorHex,
+    backgroundColor: hexCode,
+  };
+
+  const textColor = {
+    color:
+      parseInt(hexCode.replace('#', ''), 16) > 0xffffff / 1.1
+        ? 'black'
+        : 'white',
   };
 
   return (
     <View style={[styles.colorBox, boxColor]}>
-      <Text style={styles.text}>{`${colorName}: ${ColorHex}`}</Text>
+      <Text style={[styles.text, textColor]}>{`${colorName}: ${hexCode}`}</Text>
     </View>
   );
 };
@@ -16,9 +23,9 @@ const ColorBox = ({ colorName, ColorHex }) => {
 const styles = StyleSheet.create({
   colorBox: {
     paddingVertical: 10,
+    marginBottom: 10,
     width: '100%',
     textAlign: 'center',
-    color: 'white',
     fontWeight: 'bold',
     borderRadius: 3,
     alignItems: 'center',
