97aa9697b08782ef6a58f18a4cbfeb577d2124b7
Gilson Alencar <gilson.alencar94@gmail.com>
2023-10-11 23:24:17 -0300

    navigation exercise 1

--- App.js
+++ App.js
@@ -1,59 +1,20 @@
 import React from 'react';
-import { View, Text, StyleSheet, FlatList } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import ColorBox from './components/ColorBox';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './screens/Home';
+import ColorPalette from './screens/ColorPalette';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const COLORS = [
-  { colorName: 'Base03', hexCode: '#002b36' },
-  { colorName: 'Base02', hexCode: '#073642' },
-  { colorName: 'Base01', hexCode: '#586e75' },
-  { colorName: 'Base00', hexCode: '#657b83' },
-  { colorName: 'Base0', hexCode: '#839496' },
-  { colorName: 'Base1', hexCode: '#93a1a1' },
-  { colorName: 'Base2', hexCode: '#eee8d5' },
-  { colorName: 'Base3', hexCode: '#fdf6e3' },
-  { colorName: 'Yellow', hexCode: '#b58900' },
-  { colorName: 'Orange', hexCode: '#cb4b16' },
-  { colorName: 'Red', hexCode: '#dc322f' },
-  { colorName: 'Magenta', hexCode: '#d33682' },
-  { colorName: 'Violet', hexCode: '#6c71c4' },
-  { colorName: 'Blue', hexCode: '#268bd2' },
-  { colorName: 'Cyan', hexCode: '#2aa198' },
-  { colorName: 'Green', hexCode: '#859900' },
-];
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <SafeAreaView style={styles.safeArea}>
-      <FlatList
-        data={COLORS}
-        renderItem={({ item }) => (
-          <ColorBox colorName={item.colorName} hexCode={item.hexCode} />
-        )}
-        keyExtractor={(item) => item.hexCode}
-        ListHeaderComponent={<Text style={styles.heading}>Solarized</Text>}
-      />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ColorPalette" component={ColorPalette} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    paddingHorizontal: 20,
-    gap: 10,
-  },
-  heading: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    paddingTop: 30,
-  },
-  safeArea: {
-    flex: 1,
-  },
-  listContainer: {
-    width: '100%',
-  },
-});
 
 export default App;
